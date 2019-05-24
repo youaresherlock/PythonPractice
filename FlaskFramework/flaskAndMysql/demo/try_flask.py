6c14663cf747923170693c4e3f1ffcad0b7fc5c6
clarence <2263194561@qq.com>
2019-05-24 21:47:03 +0800

    "flaskAndMysql code"

---
+++ FlaskFramework/flaskAndMysql/demo/try_flask.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World"
+
+if __name__ == "__main__":
+    # * Running on http://localhost:5000/
+    app.run(debug = True)
