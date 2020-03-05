6800c64ea9f331cb8aac3234586022536c78da6c
clarence <2263194561@qq.com>
2020-03-05 09:26:58 +0800

    多进程和多线程

---
+++ python97/chapter11/progress_test.py
@@ -0,0 +1,41 @@
+#!usr/bin/python
+# -*- coding:utf8 -*-
+# 多进程编程
+# 耗cpu的操作， 用多进程编程 对于io操作来说，使用多线程编程
+
+# 1.对于耗费cpu的操作，多进程优于多线程
+import time
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ProcessPoolExecutor
+
+def fib(n):
+    if n <= 2:
+        return 1
+    return fib(n-1) + fib(n-2)
+
+# if __name__ == "__main__":
+#     # with ThreadPoolExecutor(3) as executor:
+#     with ProcessPoolExecutor(3) as executor:
+#         all_task = [executor.submit(fib, (num))for num in range(25, 40)]
+#         start_time = time.time()
+#         for future in as_completed(all_task):
+#             data = future.result()
+#             print("exe result: {}".format(data))
+#
+#         print("last time is: {}".format(time.time() - start_time))
+
+# 2.对于io操作来说，多线程优于多进程
+def random_sleep(n):
+    time.sleep(n)
+    return n
+
+if __name__ == "__main__":
+    with ThreadPoolExecutor(3) as executor:
+    # with ProcessPoolExecutor(3) as executor:
+        all_task = [executor.submit(random_sleep, (num))for num in [2]*30]
+        start_time = time.time()
+        for future in as_completed(all_task):
+            data = future.result()
+            print("exe result: {}".format(data))
+
+        print("last time is: {}".format(time.time() - start_time))
