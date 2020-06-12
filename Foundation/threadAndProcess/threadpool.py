87796dc203536aac53473a001d4d8b684123b56f
clarence <2263194561@qq.com>
2020-06-12 21:56:21 +0800

    threadpool

--- Foundation/threadAndProcess/threadpool.py
+++ Foundation/threadAndProcess/threadpool.py
@@ -1,2 +1,18 @@
 #!usr/bin/python
 # -*- coding:utf8 -*-
+from concurrent.futures import ThreadPoolExecutor
+import time
+
+
+def return_after_2_secs(message):
+    time.sleep(2)
+    return message
+
+
+pool = ThreadPoolExecutor(3)
+future = pool.submit(return_after_2_secs, message='hello')
+print(future.done())
+time.sleep(3)
+print(future.done())
+print('Result: ', future.result())
+
