863362c6b8f3ac8ee9e06f885d1be898efa042ff
clarence <2263194561@qq.com>
2020-06-20 15:31:10 +0800

    闭包的延迟绑定

--- Foundation/memorymanage/classdecorator.py
+++ Foundation/memorymanage/classdecorator.py
@@ -5,9 +5,20 @@
     1. 函数嵌套定义
     2. 内部函数使用外部函数作用域内的变量
     3. 外部函数要有返回值,返回内部函数名
+外函数在返回内函数的引用，发现内函数使用了外函数的局部变量
+该局部变量不能被销毁, 而是和内函数的引用进行绑定，存储在一个特定的空间,该空间称为闭包
+
+绑定的时机: 外函数返回时，此时局部变量的值已经固定
 """
 
 
+"""
+def func():
+    temp = [lambda x: x * i for i in range(4)]
+    return temp 
+for each in func():
+    print(each(2))
+"""
 # 函数装饰器
 def func_out(func):
     def func_in(*args, **kwargs):
@@ -40,8 +51,6 @@ def popular():
 
 
 popular()
-
-
 
 
 
