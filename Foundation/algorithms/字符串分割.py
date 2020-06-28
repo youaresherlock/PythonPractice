241b771962ad802b4cadda68e583e1ddb15c4385
clarence <2263194561@qq.com>
2020-06-28 10:01:33 +0800

    算法: fix字符串分割

--- Foundation/algorithms/字符串分割.py
+++ Foundation/algorithms/字符串分割.py
@@ -5,45 +5,33 @@
 (输出次数为N,字符串长度小于100)。
 按长度为8拆分每个字符串后输出到新的字符串数组，
 长度不是8整数倍的字符串请在后面补数字0，空字符串不处理。
-Input:
-2
-abc
-123456789
-Output:
-abc00000
-12345678
-90000000
+5
+wx
+x54on1s73ubb9c
+f29iiqb28l72k
+y
+y5vor
+--------
+wx000000
+x54on1s7
+3ubb9c00
+f29iiqb2
+8l72k000
+y0000000
+y5vor000
 """
 
 n = int(input())
 
-buf = ""
 for i in range(0, n):
     x = input()
-    if i == 0 and i != n-1:
-        buf += x
-        if len(buf) <= 8:
-            buf += '0' * (8 - len(buf))
-    elif i == n-1:
-        buf += x
-        c = len(buf) % 8
-        buf += '0' * (8 - c)
+    if len(x) < 8:
+        print(x + '0' * (8 - len(x)))
     else:
-        c = len(buf) % 8
-        if c + len(x) <= 8:
-            buf += x + '0' * (8 - c - len(x))
-        else:
-            if i == (n - 1):
-                buf += x + '0' * (16 - len(x) - c)
-            else:
-                buf += x
-while len(buf) >= 8:
-    print(buf[:8])
-    buf = buf[8:]
-print(buf)
-
-
-
+        while len(x) > 8:
+            print(x[:8])
+            x = x[8:]
+        print(x + '0' * (8 - len(x)))
 
 
 
