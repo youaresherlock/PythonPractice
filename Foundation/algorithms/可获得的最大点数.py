792aa4706c9d7460cd029953aae7d8239df5a504
clarence <2263194561@qq.com>
2020-11-21 10:34:42 +0800

    fix可以获得最大点数算法题

--- Foundation/algorithms/可获得的最大点数.py
+++ Foundation/algorithms/可获得的最大点数.py
@@ -3,13 +3,12 @@
 from typing import List
 
 
-def max_point(points: List, k: int) -> int:
+def max_point(points: List[int], k: int) -> int:
     max_index, max_sum = k, 0
     for i in range(max_index + 1):
         another_index = max_index - i
         left_list = points[:i]
-        right_list = list(reversed(points))[:another_index]
-        print(left_list, right_list, i)
+        right_list = points[-another_index:] if another_index > 0 else []
         sum_points = sum(left_list + right_list)
         if max_sum < sum_points:
             max_sum = sum_points
@@ -18,5 +17,5 @@ def max_point(points: List, k: int) -> int:
 
 
 if __name__ == '__main__':
-    points = [1, 2, 3, 4, 5, 6, 1]
-    print(max_point(points, 3))
+    message = [1, 2, 3, 4, 5, 6, 1]
+    print(max_point(message, 3))
